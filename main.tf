730d9c82ed8f0bb204f81f176e6420031f609752
raj-830803 <rajarshimu@google.com>
2026-02-13 13:45:19 +0000

    First Commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+# resource "google_project_organization_policy" "require_cloud_run_auth" {
+#   project    = var.project_id
+#   constraint = "constraints/run.managed.requireInvokerIam"
+
+#   boolean_policy {
+#     enforced = true
+#   }
+# }
+
+
+
+resource "google_org_policy_policy" "require_cloud_run_auth" {
+  # The name field for V2 must follow this exact pattern:
+  name   = "projects/${var.project_id}/policies/run.managed.requireInvokerIam"
+  parent = "projects/${var.project_id}"
+
+  spec {
+    rules {
+      enforce = "TRUE"
+    }
+  }
+}
---
+++ provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.3"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.64, < 8"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">= 4.64, < 8"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+}
